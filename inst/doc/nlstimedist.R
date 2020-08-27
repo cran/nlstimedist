4647160c81721f0803b949d5f94ecd5bbb02f0bc
Nathan Eastwood <nathan.eastwood@icloud.com>
2020-08-27 07:20:03 +0000

    version 2.0.0

--- inst/doc/nlstimedist.R
+++ inst/doc/nlstimedist.R
@@ -1,90 +1,87 @@
-## ---- include = FALSE----------------------------------------------------
+## ---- include = FALSE---------------------------------------------------------
 library(nlstimedist)
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = NA
 )
 
-## ----viewTilia-----------------------------------------------------------
+## ----viewTilia----------------------------------------------------------------
 head(lobelia)
 
-## ----cleanTilia----------------------------------------------------------
-tdLobelia <- tdData(lobelia, x = "Day", y = "Germination", 
-                    group = "Temperature")
+## ----cleanTilia---------------------------------------------------------------
+tdLobelia <- tdData(lobelia, x = "Day", y = "Germination", group = "Temperature")
 tdLobelia
 
-## ----fitModel------------------------------------------------------------
-# Fitting the model to data already in the format x = time and y = proportion 
+## ----fitModel-----------------------------------------------------------------
+# Fitting the model to data already in the format x = time and y = proportion
 # of cumulative number of events.
-lobelia12_5 <- tdLobelia$clean[tdLobelia$clean$Temperature == 12.5, ]
-model12_5 <- timedist(lobelia12_5, x = "Day", y = "propMax", 
-                      r = 0.03, c = 0.5, t = 14.5)
+lobelia12_5 <- tdLobelia[tdLobelia$Temperature == 12.5, ]
+model12_5 <- timedist(
+  lobelia12_5, x = "Day", y = "propMax", r = 0.03, c = 0.5, t = 14.5
+)
 model12_5
 
-## ----fixVals-------------------------------------------------------------
-modelFix <- timedist(data = lobelia12_5, x = "Day", y = "propMax", 
-                     r = 0.03, c = 0.5, t = 14.5, 
-                     upper = c(0.1, Inf, Inf), lower = c(0.01, -Inf, -Inf))
+## ----fixVals------------------------------------------------------------------
+modelFix <- timedist(
+  data = lobelia12_5, x = "Day", y = "propMax", r = 0.03, c = 0.5, t = 14.5,
+  upper = c(0.1, Inf, Inf), lower = c(0.01, -Inf, -Inf)
+)
 modelFix
 
-## ----standErrors---------------------------------------------------------
+## ----standErrors--------------------------------------------------------------
 summary(model12_5, correlation = TRUE, symbolic.cor = FALSE)
 
-## ----cors----------------------------------------------------------------
+## ----cors---------------------------------------------------------------------
 cpe <- vcov(model12_5)
 cov2cor(cpe)
 
-## ----confint-------------------------------------------------------------
+## ----confint------------------------------------------------------------------
 confint2(model12_5)
 
-## ----rss-----------------------------------------------------------------
+## ----rss----------------------------------------------------------------------
 tdRSS(model12_5)
 
-## ----moments-------------------------------------------------------------
+## ----moments------------------------------------------------------------------
 model12_5$m$getMoments()
 
-## ----singlePerc----------------------------------------------------------
+## ----singlePerc---------------------------------------------------------------
 # Extracting a single percentile
 tdPercentiles(model12_5, n = 0.01)
 # Extracting a sequence of percentiles from 10% to 90% in steps of 10.
 tdPercentiles(model12_5, n = seq(0.1, 0.9, 0.1))
 
-## ----singlePdf-----------------------------------------------------------
+## ----singlePdf----------------------------------------------------------------
 tdPdfPlot(model12_5, S = 1, xVals = seq(0, 30, 0.01))
 
-## ----multPdf-------------------------------------------------------------
+## ----multPdf------------------------------------------------------------------
 # Extract the individual data
-lobelia9_8 <- tdLobelia$clean[tdLobelia$clean$Temperature == 9.8, ]
-lobelia16_7 <- tdLobelia$clean[tdLobelia$clean$Temperature == 16.7, ]
-lobelia20_2 <- tdLobelia$clean[tdLobelia$clean$Temperature == 20.2, ]
-lobelia24_3 <- tdLobelia$clean[tdLobelia$clean$Temperature == 24.3, ]
-lobelia28_5 <- tdLobelia$clean[tdLobelia$clean$Temperature == 28.5, ]
-lobelia32 <- tdLobelia$clean[tdLobelia$clean$Temperature == 32, ]
+lobelia9_8 <- tdLobelia[tdLobelia$Temperature == 9.8, ]
+lobelia16_7 <- tdLobelia[tdLobelia$Temperature == 16.7, ]
+lobelia20_2 <- tdLobelia[tdLobelia$Temperature == 20.2, ]
+lobelia24_3 <- tdLobelia[tdLobelia$Temperature == 24.3, ]
+lobelia28_5 <- tdLobelia[tdLobelia$Temperature == 28.5, ]
+lobelia32 <- tdLobelia[tdLobelia$Temperature == 32, ]
 
 # Create the models
-model9_8 <- timedist(lobelia9_8, x = "Day", y = "propMax", 
-                     r = 0.1, c = 0.5, t = 25)
-model16_7 <- timedist(lobelia16_7, x = "Day", y = "propMax", 
-                      r = 0.1, c = 0.5, t = 10)
-model20_2 <- timedist(lobelia20_2, x = "Day", y = "propMax", 
-                      r = 0.1, c = 0.5, t = 10)
-model24_3 <- timedist(lobelia24_3, x = "Day", y = "propMax", 
-                      r = 0.1, c = 1, t = 5)
-model28_5 <- timedist(lobelia28_5, x = "Day", y = "propMax", 
-                      r = 0.1, c = 1, t = 5)
-model32 <- timedist(lobelia32, x = "Day", y = "propMax", 
-                    r = 0.1, c = 0.5, t = 5)
+model9_8 <- timedist(lobelia9_8, x = "Day", y = "propMax", r = 0.1, c = 0.5, t = 25)
+model16_7 <- timedist(lobelia16_7, x = "Day", y = "propMax", r = 0.1, c = 0.5, t = 10)
+model20_2 <- timedist(lobelia20_2, x = "Day", y = "propMax", r = 0.1, c = 0.5, t = 10)
+model24_3 <- timedist(lobelia24_3, x = "Day", y = "propMax", r = 0.1, c = 1, t = 5)
+model28_5 <- timedist(lobelia28_5, x = "Day", y = "propMax", r = 0.1, c = 1, t = 5)
+model32 <- timedist(lobelia32, x = "Day", y = "propMax", r = 0.1, c = 0.5, t = 5)
 
 # Generate the plot
-tdPdfPlot(model9_8, model12_5, model16_7, model20_2, model24_3, model28_5, 
-          model32, 
-          S = c(0.213, 0.307, 0.533, 0.707, 0.867, 0.907, 0.840), 
-          xVals = seq(0, 30, 0.001))
+tdPdfPlot(
+  model9_8, model12_5, model16_7, model20_2, model24_3, model28_5, model32,
+  S = c(0.213, 0.307, 0.533, 0.707, 0.867, 0.907, 0.840),
+  xVals = seq(0, 30, 0.001)
+)
 
-## ----singleCdf-----------------------------------------------------------
+## ----singleCdf----------------------------------------------------------------
 tdCdfPlot(model12_5, S = 1, xVals = seq(0, 30, 0.01))
-tdCdfPlot(model9_8, model12_5, model16_7, model20_2, model24_3, model28_5, 
-          model32, 
-          S = c(0.213, 0.307, 0.533, 0.707, 0.867, 0.907, 0.840), 
-          xVals = seq(0, 30, 0.001))
+tdCdfPlot(
+  model9_8, model12_5, model16_7, model20_2, model24_3, model28_5, model32,
+  S = c(0.213, 0.307, 0.533, 0.707, 0.867, 0.907, 0.840),
+  xVals = seq(0, 30, 0.001)
+)
 
